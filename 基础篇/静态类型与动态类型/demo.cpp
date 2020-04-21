1d3fc4366a291414c6eb5ea2c4286e252979a774
Lyn Ho <lyn4develop@gmail.com>
2020-04-21 23:04:01 +0800

    强类型与弱类型

---
+++ 基础篇/静态类型与动态类型/demo.cpp
@@ -0,0 +1,9 @@
+class C {
+  public:
+    int x;
+    int y;
+}
+
+int add(C a, C b) {
+  return a.x + a.y + b.x + b.y
+}
---
+++ 基础篇/静态类型与动态类型/demo.js
@@ -0,0 +1,10 @@
+class C {
+  constructor(x, y) {
+    this.x = x
+    this.y = y
+  }
+}
+
+function add(a, b) {
+  return a.x + a.y + b.x + b.y
+}
