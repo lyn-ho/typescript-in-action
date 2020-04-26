ab25c1aa5437a753a410395f6b3b2cb8d007b53c
Lyn Ho <lyn4develop@gmail.com>
2020-04-26 22:20:00 +0800

    类 — 继承和修饰符

---
+++ demo/src/class.ts
@@ -0,0 +1,39 @@
+class Dog {
+  constructor(name: string) {
+    this.name = name
+  }
+
+  public name: string
+
+  run() { }
+
+  private pri() { }
+
+  protected pro() { }
+
+  readonly legs: number = 4
+
+  static food: string = 'bones'
+}
+
+console.log(Dog.prototype)
+
+let dog = new Dog('ww')
+
+console.log(dog)
+
+// dog.pri() // error
+// dog.pro() // error
+
+class Husky extends Dog {
+  constructor(name : string, public color: string) {
+    super(name)
+
+    this.color = color
+
+    // this.pri()  // error
+    this.pro()
+  }
+
+  // color: string
+}
